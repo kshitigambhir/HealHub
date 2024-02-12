56ee79269c520379881157a24fae05c55c765685
kshitigambhir <134823553+kshitigambhir@users.noreply.github.com>
2024-02-12 18:22:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    // Implement logout functionality here
+    alert('Logout functionality will be implemented here');
+  });
